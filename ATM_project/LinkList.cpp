df3249aaf94de76976a88c08bb2671df3f5427da
thangdot <70512561+thangdot@users.noreply.github.com>
2020-10-13 20:58:54 +0700

    Thêm class LinkList

---
+++ ATM_project/LinkList.cpp
@@ -0,0 +1,23 @@
+﻿#include "LinkList.h"
+#include <iostream>
+
+using namespace std;
+
+/***********************************************************
+* @Depcription Khởi tạo một Node mới có giá trị là data
+***********************************************************/
+template <class Datatype>
+Node <Datatype>::Node(Datatype _data)
+{
+	_data = data;
+	_pNext = NULL;
+}
+
+/***********************************************************
+* @Depcription Hiển thị giá trị Node
+***********************************************************/
+template <class Datatype>
+void Node <Datatype>::display()
+{
+	cout << _data << end;
+}
---
+++ ATM_project/LinkList.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+template <class Datatype>
+
+class Node {
+private:
+	Datatype _data;	// Giá trị của con trỏ Node
+	Node* _pNext;		// Con trỏ đến Node kế tiếp
+
+public:
+	Node(Datatype _data);	// Khởi tạo node mới có giá trị là Data
+	void display();	// Hiển thị giá trị của Node
+};
